588ecb80250e29edb98651dc2947c30b9b2eee81
Andreas Syrén <>
2019-01-23 23:49:21 +0100

    Made sure MANIFEST is used

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/atomichighfive/tealeaves",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
           'pandas',
           'numpy',
