03844d46eacada2a644d180c43c8e8b5bedbb62e
Andreas Syrén <>
2019-01-23 23:12:45 +0100

    Added dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/atomichighfive/tealeaves",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'pandas',
+          'numpy',
+          'scipy',
+          'seaborn',
+          'matplotlib',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
