44730bd53885f4f24e67d887e066de38ac80f08d
Andreas Syrén <>
2019-02-07 09:31:23 +0100

    version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tealeaves-atomichighfive",
-    version="1.0.0",
+    version="1.1.0",
     author="Andreas Syrén",
     author_email="filipandreassyren@gmail.com",
     description="No-mental-effort data exploration toolbox",
